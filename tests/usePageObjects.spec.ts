9e61ce60d7507e42ad00d233659d9a6b1e366aa5
mgarcia3-godaddy <mgarcia3@godaddy.com>
2025-09-14 22:13:24 -0600

    Changed file name

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { PageManager } from '../page-objects/pageManager';
-import  {faker } from '@faker-js/faker';
+import {faker} from '@faker-js/faker';
 
 test.beforeEach(async ({page}) => {
     await page.goto('/');
@@ -17,22 +17,22 @@ test('Navigate to form page @smoke @regression', async ({page}) => {
 
 test('Parametrized Methods @smoke', async ({page}) => {
     const pm = new PageManager(page);
-    const randomfullName = faker.person.fullName();
-    const randomEmail = `${randomfullName.replace(' ', '')}${faker.number.int(1000)}@test.com`;
+    const randomFullName = faker.person.fullName();
+    const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`;
 
     await pm.navigateTo().formLayoutsPage();
     await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption(process.env.USERNAME1, process.env.PASSWORD1, 'Option 1');
     await page.screenshot({path: `screenshots/formsLayoutsPage.png`});
     const buffer = await page.screenshot();
     console.log(buffer.toString('base64'));
-    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox(randomfullName, randomEmail, true);
+    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, true);
     await page.locator('nb-card', {hasText: 'Inline form'}).screenshot({path: `screenshots/inlineForm.png`});
     await pm.navigateTo().datepickerPage();
     await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10);
     await pm.onDatepickerPage().selectDatePickerWithRangeFromToday(7, 14);
 });
 
-test.only('testing with argos ci', async ({page}) => {
+test('testing with argos ci', async ({page}) => {
     const pm = new PageManager(page);
     await pm.navigateTo().formLayoutsPage();
     await pm.navigateTo().datepickerPage();
