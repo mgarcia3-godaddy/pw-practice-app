1822f1df9d7aa3cab8b64d45a375ee45d09eb960
mgarcia3-godaddy <mgarcia3@godaddy.com>
2025-09-14 21:38:56 -0600

    added new test

--- tests/001-usePageObjects.spec.ts
+++ tests/001-usePageObjects.spec.ts
@@ -31,3 +31,9 @@ test('Parametrized Methods @smoke', async ({page}) => {
     await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10);
     await pm.onDatepickerPage().selectDatePickerWithRangeFromToday(7, 14);
 });
+
+test.only('testing with argos ci', async ({page}) => {
+    const pm = new PageManager(page);
+    await pm.navigateTo().formLayoutsPage();
+    await pm.navigateTo().datepickerPage();
+});
